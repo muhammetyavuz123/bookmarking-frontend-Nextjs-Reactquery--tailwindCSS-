bfe183df8003a6d69dc635e412e063e1b7c8ddbc
muhammetyavuz123 <muhametyavuz@hotmail.com>
2022-02-12 23:27:53 +0300

    added search

--- src/components/organisms/Main/CardsSection/index.tsx
+++ src/components/organisms/Main/CardsSection/index.tsx
@@ -5,15 +5,25 @@ import { fetchProducts } from "../../../../apis";
 
 export const CardsSection: FC = ({}) => {
   const [page, setPage] = useState(1);
+  const [search, setSearch] = useState([]);
   const queryClient = useQueryClient();
 
+  // data get react query
   const { status, data, error, isFetching, isPreviousData } = useQuery<
     products[]
   >(["products", page], () => fetchProducts(page), {
     keepPreviousData: true,
     staleTime: 5000,
   });
-  console.log("data", data);
+
+  // search function
+  const filtered = data?.filter((produc) =>
+    produc.title.toLowerCase().includes(search?.toString().toLowerCase())
+  );
+
+  const searchProduc = (event: any) => {
+    setSearch(event.target.value);
+  };
 
   useEffect(() => {
     if (data) {
@@ -32,9 +42,37 @@ export const CardsSection: FC = ({}) => {
       ) : (
         ""
       )}
-      <div className="container mx-auto">
-        <div className=" grid gap-8 grid-cols-1 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1">
-          {data?.map((item) => {
+      <div className="container mx-auto ">
+        <div className=" grid gap-8 grid-cols-1 lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1  ">
+          <div className="relative text-gray-600 focus-within:text-gray-400">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+              <button
+                type="submit"
+                className="p-1 focus:outline-none focus:shadow-outline"
+              >
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  className="w-6 h-6"
+                >
+                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                </svg>
+              </button>
+            </span>
+            <input
+              type="search"
+              name="q"
+              className="py-2 text-sm text-white bg-gray-900 rounded-md pl-10 focus:outline-none  w-full h-14"
+              placeholder="Search..."
+              autoComplete="off"
+              onChange={searchProduc}
+            />
+          </div>
+          {filtered?.map((item) => {
             return (
               <Card
                 key={item._id}
